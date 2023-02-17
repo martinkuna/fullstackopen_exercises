c6981c2874613807291c610b5e82e7aa82096299
Martin Kuna <martinkuna@users.noreply.github.com>
2023-02-17 16:05:21 -0700

    part1: Exercise 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -10,7 +10,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -19,9 +19,9 @@ const Content = (props) => {
   return (
     // Here I use a react fragment, instead of adding extra nodes to the DOM
     <> 
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part3} />
+      <Part part={props.part2} />
     </>
   )
 }
@@ -36,18 +36,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercise_total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercise_total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
